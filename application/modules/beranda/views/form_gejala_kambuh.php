cf229fddacc4b6089f8eba49a549070ce06385d7
bebbyilham <bebbyilham@gmail.com>
2023-05-11 11:16:04 +0700

    110523 1115 tgl input

--- application/modules/beranda/views/form_gejala_kambuh.php
+++ application/modules/beranda/views/form_gejala_kambuh.php
@@ -7,7 +7,7 @@
             <h6 class="h2 text-white d-inline-block mb-0"><?= $title; ?></h6>
           </div>
           <div class="col-lg-6 col-5 text-right">
-            <a href="<?php echo base_url(); ?>beranda/jiwakeluarga" class="btn btn-sm btn-neutral">Kembali</a>
+            <a href="<?php echo base_url(); ?>beranda/gejalakambuh" class="btn btn-sm btn-neutral">Kembali</a>
           </div>
         </div>
         <!-- Card stats -->
